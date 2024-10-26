730370580ec52c574f69838770fdbc0df1f6cd66
itsme123c <jaya.alex7419@gmail.com>
2024-10-26 19:44:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from bot.py import start, merge_files
 
 def main() -> None:
-  """Starts the bot."""
-  application = ApplicationBuilder().token("TELEGRAM_BOT_TOKEN").build()
+    """Starts the bot."""
+    application = ApplicationBuilder().token("TELEGRAM_BOT_TOKEN").build()
 
-  # Register handlers
-  application.add_handler(CommandHandler("start", start))
-  application.add_handler(MessageHandler(filters.Document.ALL, merge_files))
+    # Register handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.Document.ALL, merge_files))
 
-  # Run the bot
-  application.run_polling()
+    # Run the bot
+    application.run_polling()
 
 if __name__ == "__main__":
-  main()
+    main()
